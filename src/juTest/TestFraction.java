5d8b37f2951ce6945078be41a4db448392eeeec9
= <=>
2014-10-24 08:57:49 +0100

    some other changes...

---
+++ src/juTest/TestFraction.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package juTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author me
+ *
+ */
+public class TestFraction {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
