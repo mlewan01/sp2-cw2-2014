6f0361679339b06ed79c52fe25267aa42ac35374
= <=>
2014-10-24 11:36:35 +0100

    start jutesting

--- src/juTest/TestFraction.java
+++ src/juTest/TestFraction.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import frctCalc.Fraction;
+
 /**
  * @author me
  *
@@ -15,7 +17,13 @@ public class TestFraction {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		int nume = 0;
+		int denu = 1;
+		
+		Fraction f = new Fraction(nume, denu);
+		assertEquals(" is deniminator equal to zero? ", false , f.getDenominator() == 0);
+		
 	}
 
 }
