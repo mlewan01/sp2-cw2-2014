e5b66cc6fa52cfc06b922dc89926f6a07dc2ab16
= <=>
2014-11-01 16:51:24 +0000

    some more code for calculator

--- src/frctCalc/FractionCalculator.java
+++ src/frctCalc/FractionCalculator.java
@@ -3,6 +3,8 @@
  */
 package frctCalc;
 
+import java.util.Scanner;
+
 /**
  * @author: mlewan01 <Mariusz Lewandowski, Student ref: 12906023>
  * class: sp2-2014
@@ -14,6 +16,21 @@ public class FractionCalculator {
 	
 	public FractionCalculator(){
 		f = new Fraction(0,1);
+	}
+	
+	public static void main(String[] arg){
+		System.out.println("wellcome in Fraction calculator");
+		
+		boolean contin = true;
+		String s;
+		Scanner sc = new Scanner(System.in);
+		do{
+			System.out.println("Please, enter data to calculate Fraction: ");
+			
+			s=sc.next();
+
+		}while(s.contains("q") || s.contains("Q"));
+		sc.close();
 	}
 	
 	public Fraction evaluate(Fraction fr, String s){
