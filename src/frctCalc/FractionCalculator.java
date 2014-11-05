c1afa0032f0e9a44c02c87c90b52f0e077619d00
= <=>
2014-11-05 01:37:29 +0000

    some regEx-es

--- src/frctCalc/FractionCalculator.java
+++ src/frctCalc/FractionCalculator.java
@@ -4,6 +4,7 @@
 package frctCalc;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @author: mlewan01 <Mariusz Lewandowski, Student ref: 12906023>
@@ -13,25 +14,40 @@ import java.util.Scanner;
 public class FractionCalculator {
 	
 	private Fraction f;
+	String p1 = "[+-*/]";
+	String p2 = "\n";
+	String p3 = "\\d";
+	String p4 = "\\d/\\d";
+	String p5 = "[Qq]|quit|Quit";
 	
 	public FractionCalculator(){
+		Pattern p = Pattern.compile(p4);
 		f = new Fraction(0,1);
 	}
 	
 	public static void main(String[] arg){
-		System.out.println("wellcome in Fraction calculator");
+		System.out.println("wellcome in Fraction calculator  \\o/\n");
+		System.out.println("please enter data in following format: \"3/4 + 1/-3 * 7 / 5\"");
+		System.out.println("you can enter data in multiple lines (provided you know how)!");
 		
 		boolean contin = true;
-		String s;
+		String s="";
 		Scanner sc = new Scanner(System.in);
 		do{
 			System.out.println("Please, enter data to calculate Fraction: ");
 			
-			s=sc.next();
-
-		}while(s.contains("q") || s.contains("Q"));
+			
+			s = sc.nextLine();
+			sc.hasNext();
+			
+			
+			
+			contin = !(s.contains("q") || s.contains("Q"));
+		}while(contin);
 		sc.close();
-	}
+		System.out.println("main loop exited...");
+		
+	}	
 	
 	public Fraction evaluate(Fraction fr, String s){
 		System.out.println("the string is: "+s);
